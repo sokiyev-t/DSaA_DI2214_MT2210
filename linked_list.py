b6610329239e59526b5aa15512f4a2f20a7fef27
sokiyev-t <iron623@mail.ru>
2023-10-28 12:42:43 +0500

    linked _list 2

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,4 @@
+import sys
 class Node:
     def __init__(self, next=None):
         self.next=next
@@ -30,9 +31,33 @@ class LinkedList:
     def get_counter(self):
         return self.count
 
-    def get(self, id):
+    def get_val_by_id(self, id):
+        if id<self.count:
+            h=self.head
+            for i in range(id):
+                h=h.get_next()
+            return h.get_val()
+        else:
+           return None
 
+    def insert_by_id(self, id, val):
+        if id<self.count:
+            h = self.head
+            for i in range(id):
+                h = h.get_next()
+            new_node=Node()
+            new_node.set_val(val)
+            new_node.set_next(h.get_next())
+            h.set_next(new_node)
+            self.count+=1
+        else:
+            return None
 
+    def print_list(self):
+        h=self.head
+        for i in range(self.count):
+            print(h.get_val())
+            h=h.get_next()
 
 my_list=LinkedList()
 my_list.insert(12)
@@ -40,4 +65,17 @@ my_list.insert(15)
 my_list.insert(23)
 my_list.insert(75)
 my_list.insert(1)
-print(f"My list len is {my_list.get_counter()}, head is: {my_list.get_element_by_id()}")
+my_list.insert_by_id(2,33)
+my_list.print_list()
+
+test_node=Node()
+test_node.set_val(12)
+
+v=12
+s=sys.getsizeof(test_node)
+print(f"size of: {s}")
+
+# print(f"My list len is {my_list.get_counter()}, head is: {my_list.head.get_val()}")
+#
+# v=my_list.get_val_by_id(2)
+# print(f"Result is {v}")
